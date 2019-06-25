2b096cfcc8347b124f4a1b508e82e9be45f3565b
Pavel Sokolov <pavel.zeppa@yandex.ru>
2019-06-25 12:30:52 +0300

    PresenterHandler is updated
    - error handling is added

--- library/src/main/java/com/simplemvp/presenter/PresenterHandler.java
+++ library/src/main/java/com/simplemvp/presenter/PresenterHandler.java
@@ -1,5 +1,7 @@
 package com.simplemvp.presenter;
 
+import android.util.Log;
+
 import com.simplemvp.common.MvpPresenter;
 import com.simplemvp.common.MvpState;
 
@@ -9,10 +11,12 @@ import java.lang.reflect.Proxy;
 import java.util.concurrent.ExecutorService;
 
 class PresenterHandler<S extends MvpState> implements InvocationHandler {
+    private final String tag;
     private final ExecutorService executor;
     private final MvpPresenter<S> presenter;
 
-    PresenterHandler(ExecutorService executor, MvpPresenter<S> presenter) {
+    private PresenterHandler(ExecutorService executor, MvpPresenter<S> presenter) {
+        this.tag = presenter.getClass().getSimpleName();
         this.executor = executor;
         this.presenter = presenter;
     }
@@ -22,9 +26,27 @@ class PresenterHandler<S extends MvpState> implements InvocationHandler {
                 presenter.getClass().getInterfaces(), new PresenterHandler<>(executor, presenter));
     }
 
+    private static String formStackTrace(Exception e) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(e.toString());
+        builder.append('\n');
+        for (StackTraceElement element : e.getStackTrace()) {
+            builder.append('\t');
+            builder.append(element.toString());
+            builder.append('\n');
+        }
+        return builder.toString();
+    }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        executor.submit(() -> method.invoke(presenter, args));
+        executor.execute(() -> {
+            try {
+                method.invoke(presenter, args);
+            } catch (Exception e) {
+                Log.e(tag, "error: " + formStackTrace(e));
+            }
+        });
         return null;
     }
 }
