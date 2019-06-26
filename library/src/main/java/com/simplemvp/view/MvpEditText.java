f989b69cdac0494c21bdaab06a89ec2fdea22fa8
Pavel Sokolov <pavel.zeppa@yandex.ru>
2019-06-26 12:42:51 +0300

    New class is added - MvpEditText
    - this class provides a method that changes text without listeners invocation

---
+++ library/src/main/java/com/simplemvp/view/MvpEditText.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2019 Pavel A. Sokolov
+ */
+
+package com.simplemvp.view;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This class uses reflections to avoid watchers invocation when text is changed
+ */
+public class MvpEditText extends AppCompatEditText {
+    private Field mListenersField;
+    private List<TextWatcher> mListeners = Collections.emptyList();
+
+    public MvpEditText(Context context) {
+        super(context);
+        initListenersField();
+    }
+
+    public MvpEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initListenersField();
+    }
+
+    public MvpEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initListenersField();
+    }
+
+    private void initListenersField() {
+        try {
+            mListenersField = TextView.class.getDeclaredField("mListeners");
+            mListenersField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+        }
+    }
+
+    public void setTextNoWatchers(String text) {
+        try {
+            mListeners = (List<TextWatcher>) mListenersField.get(this);
+            mListenersField.set(this, new ArrayList<>());
+            setText(text);
+            setSelection(text == null || text.isEmpty() ? 0 : text.length());
+            mListenersField.set(this, mListeners);
+        } catch (IllegalAccessException e) {
+        }
+    }
+}
