a75541822128125bef097044f84c639bde1a6324
Pavel Sokolov <pavel.zeppa@yandex.ru>
2020-02-06 15:45:13 +0300

    MvpEventHandler update
    - events queue and related code are removed
    - strong reference to a parent view

--- library/src/main/java/com/simplemvp/view/MvpEventHandler.java
+++ library/src/main/java/com/simplemvp/view/MvpEventHandler.java
@@ -7,6 +7,7 @@ package com.simplemvp.view;
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.OnLifecycleEvent;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -30,50 +31,33 @@ import com.simplemvp.common.MvpPresenter;
 import com.simplemvp.common.MvpState;
 import com.simplemvp.common.MvpView;
 import com.simplemvp.common.MvpViewHandle;
-import com.simplemvp.functions.Consumer;
 
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-class MvpEventHandler<S extends MvpState, P extends MvpPresenter<S>>
+class MvpEventHandler<S extends MvpState, P extends MvpPresenter<S>> extends ContextWrapper
         implements MvpViewHandle<S>, MvpListener, LifecycleObserver {
-    private final static int QUEUE_SIZE = 8;
     private final String tag = getClass().getSimpleName();
-    private final WeakReference<MvpView<S, P>> reference;
-    private final ReferenceQueue<MvpView<S, P>> referenceQueue;
+    private final MvpView<S, P> view;
     private final P presenter;
-    private final Queue<EventRunnable> events = new ConcurrentLinkedQueue<>();
     private final Handler handler = new Handler(Looper.getMainLooper());
     private final List<TextWatcher> textWatchers = new ArrayList<>();
     private final List<SearchView.OnQueryTextListener> queryTextListeners = new ArrayList<>();
     private final AtomicBoolean isFirstStateChange = new AtomicBoolean(true);
     private final AtomicBoolean isEnabled = new AtomicBoolean();
     private final AtomicBoolean isResumed = new AtomicBoolean();
-    private final AtomicBoolean isQueueDraining = new AtomicBoolean();
-    private volatile EventRunnable lastStateEvent;
+    private S state;
 
     MvpEventHandler(MvpView<S, P> view, P presenter) {
-        this.referenceQueue = new ReferenceQueue<>();
-        this.reference = new WeakReference<>(view, referenceQueue);
+        super(view.getContext());
+        this.view = view;
         this.presenter = presenter;
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     public void onResumed() {
         isResumed.set(true);
-        if (isResumed()) {
-            if (events.isEmpty()) {
-                handleLastState();
-            } else {
-                Log.d(tag, "flushing event queue");
-                drainEventQueue();
-            }
-        }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
@@ -134,72 +118,70 @@ class MvpEventHandler<S extends MvpState, P extends MvpPresenter<S>>
     }
 
     @Override
-    public MvpView<S, ?> getMvpView() {
-        return reference.get();
+    public MvpView<S, P> getMvpView() {
+        return view;
     }
 
     @Override
     public int getLayoutId() {
-        return getMvpView().getLayoutId();
+        return view.getLayoutId();
     }
 
     @Override
     public Bundle getArguments() {
-        MvpView<S, P> view = reference.get();
-        return view == null ? new Bundle() : view.getArguments();
+        return view.getArguments() == null ? new Bundle() : view.getArguments();
     }
 
     @Override
     public void post(S state) {
-        postEvent(new StateEvent(state));
+        if (isFirstStateChange()) {
+            view.onFirstStateChange(state);
+        }
+        view.onStateChanged(state);
     }
 
     @Override
     public void finish() {
-        postEvent(new PresenterEvent(view -> view.finish()));
+        view.finish();
     }
 
     @Override
     public void showDialog(DialogFragment dialog) {
-        postEvent(new PresenterEvent(view -> view.showDialog(dialog)));
+        view.showDialog(dialog);
     }
 
     @Override
     public void showSnackBar(String text, int duration) {
-        postEvent(new PresenterEvent(view -> Snackbar.make(view.getView(), text, duration).show()));
+        Snackbar.make(view.getView(), text, duration).show();
     }
 
     @Override
     public void showSnackBar(int res, int duration) {
-        postEvent(new PresenterEvent(view -> Snackbar.make(view.getView(), res, duration).show()));
+        Snackbar.make(view.getView(), res, duration).show();
     }
 
     @Override
     public void showToast(String text, int duration) {
-        postEvent(new PresenterEvent(view ->
-                Toast.makeText(view.getContext(), text, duration).show()));
+        Toast.makeText(view.getContext(), text, duration).show();
     }
 
     @Override
     public void showToast(int resId, int duration) {
-        postEvent(new PresenterEvent(view ->
-                Toast.makeText(view.getContext(), resId, duration).show()));
+        Toast.makeText(view.getContext(), resId, duration).show();
     }
 
     @Override
     public void startActivity(Intent intent) {
-        postEvent(new PresenterEvent(view -> view.getContext().startActivity(intent)));
+        view.getContext().startActivity(intent);
     }
 
     @Override
     public void startActivityForResult(Intent intent, int requestCode) {
-        postEvent(new PresenterEvent(view -> {
-            if (view instanceof AppCompatActivity) {
-                ((AppCompatActivity) view).startActivityForResult(intent, requestCode);
-            } else {
-                Log.e(tag, "only activity can start activity for result");
-            }
-        }));
+        if (view instanceof AppCompatActivity) {
+            ((AppCompatActivity) view).startActivityForResult(intent, requestCode);
+        } else {
+            Log.e(tag, "only activity can start activity for result");
+        }
     }
 
     /**
@@ -207,32 +189,9 @@ class MvpEventHandler<S extends MvpState, P extends MvpPresenter<S>>
      * current class.
      */
     void handleLastState() {
-        if (lastStateEvent != null) {
-            lastStateEvent.run();
+        if (state != null) {
+            post(state);
         }
-    }
-
-    /**
-     * This method post event to be processed on the main thread context
-     *
-     * @param runnable event to be sent
-     */
-    synchronized private void postEvent(EventRunnable runnable) {
-        events.offer(runnable);
-        if (isResumed() && isQueueDraining.compareAndSet(false, true)) {
-            handler.post(this::drainEventQueue);
-        }
-        expungeStaleEntries();
-    }
-
-    /**
-     * This method polls event from the queue and updates flag
-     *
-     * @return event or null
-     */
-    synchronized private EventRunnable pollEvent() {
-        isQueueDraining.compareAndSet(true, !events.isEmpty());
-        return events.poll();
     }
 
     /**
@@ -244,8 +203,7 @@ class MvpEventHandler<S extends MvpState, P extends MvpPresenter<S>>
      */
     boolean setEnabled(boolean enabled) {
         boolean isChanged = isEnabled.compareAndSet(!enabled, enabled);
-        if (isChanged && isResumed()) {
-            onResumed();
+        if (isChanged) {
         }
         return isChanged;
     }
@@ -254,87 +212,8 @@ class MvpEventHandler<S extends MvpState, P extends MvpPresenter<S>>
         return isEnabled.get() && isResumed.get();
     }
 
-    private void drainEventQueue() {
-        int size = events.size();
-        int n = size / QUEUE_SIZE;
-        // drainEventQueue may be called when View has been paused or it is about to be destroyed
-        // so it is better to check this flag before start state processing
-        while (isResumed()) {
-            EventRunnable event = pollEvent();
-            if (event == null) {
-                break;
-            }
-            // process every n'th state in case of queue overflow
-            if (n == 0 || size % n == 0) {
-                if (event.isState()) {
-                    lastStateEvent = event;
-                }
-                event.run();
-                size = events.size();
-                n = size / QUEUE_SIZE;
-            }
-        }
-    }
-
-    private void expungeStaleEntries() {
-        synchronized (referenceQueue) {
-            if (referenceQueue.poll() != null) {
-                presenter.disconnect(this);
-            }
-        }
-    }
-
     private boolean isFirstStateChange() {
         return isFirstStateChange.getAndSet(false);
     }
 
-    private interface EventRunnable extends Runnable {
-        default boolean isState() {
-            return false;
-        }
-    }
-
-    private class StateEvent implements EventRunnable {
-        final S state;
-
-        StateEvent(S state) {
-            this.state = state;
-        }
-
-        @Override
-        public boolean isState() {
-            return true;
-        }
-
-        @Override
-        public void run() {
-            MvpView<S, P> view = reference.get();
-            if (view != null) {
-                if (isFirstStateChange()) {
-                    view.onFirstStateChange(state);
-                }
-                view.onStateChanged(state);
-            }
-        }
-    }
-
-    private class PresenterEvent implements EventRunnable {
-        final Consumer<MvpView<S, ?>> consumer;
-
-        PresenterEvent(Consumer<MvpView<S, ?>> consumer) {
-            this.consumer = consumer;
-        }
-
-        @Override
-        public void run() {
-            MvpView<S, P> view = reference.get();
-            if (view != null) {
-                try {
-                    consumer.accept(view);
-                } catch (Exception e) {
-                    Log.e(tag, "error:", e);
-                }
-            }
-        }
-    }
 }
