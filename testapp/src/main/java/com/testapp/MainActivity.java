779cab96984e9c7fa6f36dfd679db6d5c784ff36
Pavel Sokolov <pavel.zeppa@yandex.ru>
2020-02-10 14:06:13 +0300

    MainActivity update
    - hide keyboard when events page is selected

--- testapp/src/main/java/com/testapp/MainActivity.java
+++ testapp/src/main/java/com/testapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.testapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -7,14 +8,17 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.util.Consumer;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.inputmethod.InputMethodManager;
 
 import com.simplemvp.common.MvpPresenter;
 import com.simplemvp.presenter.MvpPresenterManager;
 import com.simplemvp.view.MvpActivity;
 
 public class MainActivity extends MvpActivity<MvpPresenter<MainState>, MainState> {
+    private InputMethodManager imm;
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -38,6 +42,7 @@ public class MainActivity extends MvpActivity<MvpPresenter<MainState>, MainState
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         viewPager = findViewById(R.id.view_pager);
@@ -45,6 +50,11 @@ public class MainActivity extends MvpActivity<MvpPresenter<MainState>, MainState
         tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
         clearAll = findViewById(R.id.clear_all);
+        viewPager.addOnPageChangeListener(new OnPageSelected(i -> {
+            if (i == 1) {
+                imm.hideSoftInputFromWindow(getView().getApplicationWindowToken(), 0);
+            }
+        }));
     }
 
     @Override
@@ -84,6 +94,29 @@ public class MainActivity extends MvpActivity<MvpPresenter<MainState>, MainState
         @Override
         public int getCount() {
             return 2;
+        }
+    }
+
+    private class OnPageSelected implements ViewPager.OnPageChangeListener {
+        private final Consumer<Integer> consumer;
+
+        public OnPageSelected(Consumer<Integer> consumer) {
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            consumer.accept(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
         }
     }
 }
